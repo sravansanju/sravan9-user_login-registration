a843fefb1d4cd6e0fa57d434c758df9341112efe
sravan sanju <kudurusravan24@gmail.com>
2023-01-07 07:30:11 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,70 @@
+
+from django.shortcuts import render
+from app.forms import *
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+# Create your views here.
+from django.core.mail import send_mail
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+
+
+
+def registration(request):
+    uf=UserForm()
+    pf=ProfileForm()
+    d={'uf':uf,'pf':pf}
+    if request.method=='POST' and request.FILES:
+        UD=UserForm(request.POST)
+        PD=ProfileForm(request.POST,request.FILES)
+        if UD.is_valid() and PD.is_valid():
+            pw=UD.cleaned_data['password']
+            USO=UD.save(commit=False)
+            USO.set_password(pw)
+            USO.save()
+
+            PFO=PD.save(commit=False)
+            PFO.user=USO
+            PFO.save()
+
+            send_mail('User registration',
+            'Regiistration is success full',
+            'kumarsanjusravan@gmail.com',
+            [USO.email],fail_silently=False)
+
+
+            return HttpResponse('Registration is Successfull')
+    return render(request,'registration.html',d)
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']        
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user and user.is_active:
+            login(request,user)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+
+
+
+
+    return render(request,'user_login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+
+
